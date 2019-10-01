7b056415dabd0d5aa0c83d76af63ccd29663aeeb
sauzy34 <sonugupta34@gmail.com>
2019-10-01 17:46:46 +0530

    wip: examples

---
+++ components/example/metro.config.js
@@ -0,0 +1,23 @@
+const blacklist = require('metro-config/src/defaults/blacklist')
+const path = require('path')
+const pkg = require('./package.json')
+
+const glob = require('glob-to-regexp')
+
+function getBlacklist() {
+  const nodeModuleDirs = [
+    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
+    glob(`${path.resolve(__dirname, '..')}/e2e/*`),
+    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
+    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/hoist-non-react-statics/*`)
+  ]
+  return blacklist(nodeModuleDirs)
+}
+
+module.exports = {
+  resolver: {
+    blacklistRE: getBlacklist(),
+    providesModuleNodeModules: Object.keys(pkg.dependencies)
+  },
+  watchFolders: [path.resolve(__dirname, '..')]
+}
---
+++ components/example/pagerAndDrawer/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Platform } from 'react-native'
+
+import { DrawerLayoutAndroid, createNativeWrapper } from 'react-native-gesture-handler'
+import ViewPagerAndroid from '@react-native-community/viewpager'
+
+const WrappedViewPagerAndroid = createNativeWrapper(ViewPagerAndroid, {
+  disallowInterruption: true
+})
+
+const Page = ({ backgroundColor, text }) => (
+  <View style={[styles.page, { backgroundColor }]}>
+    <Text style={styles.pageText}>{text}</Text>
+  </View>
+)
+
+export default class Example extends Component {
+  render() {
+    if (Platform.OS !== 'android') {
+      return <Text>Sorry, this is a demo of android-only native components</Text>
+    }
+    const navigationView = (
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <Text style={{ margin: 10, fontSize: 15, textAlign: 'left' }}>I'm in the Drawer!</Text>
+      </View>
+    )
+    return (
+      <WrappedViewPagerAndroid style={styles.container}>
+        <View>
+          <DrawerLayoutAndroid
+            drawerWidth={200}
+            drawerPosition={DrawerLayoutAndroid.positions.Left}
+            renderNavigationView={() => navigationView}>
+            <Page backgroundColor="gray" text="First 🙈" />
+          </DrawerLayoutAndroid>
+        </View>
+        <Page backgroundColor="yellow" text="Second 🙉" />
+        <Page backgroundColor="blue" text="Third 🙊" />
+        <View>
+          <DrawerLayoutAndroid
+            drawerWidth={200}
+            drawerPosition={DrawerLayoutAndroid.positions.Right}
+            renderNavigationView={() => navigationView}>
+            <Page backgroundColor="blue" text="Fourth 😎" />
+          </DrawerLayoutAndroid>
+        </View>
+      </WrappedViewPagerAndroid>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 20 : 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  page: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  pageText: {
+    fontSize: 21,
+    color: 'white'
+  }
+})
